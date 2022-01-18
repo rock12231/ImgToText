c343d8623a385974a670fa4b6ab5ed82ba274a22
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-01-18 19:37:09 +0530

    new

--- readImg/settings.py
+++ readImg/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
--- tesseract/urls.py
+++ tesseract/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from tesseract import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
-       path('', views.index, name='homepage'),
-]
+    path('', views.index, name='homepage'),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+] 
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
